1d59db60142d7d3aa53889c54d9f08766f30c4bc
Александр Жаворонок <C0baltt@live.com>
2021-08-10 21:03:16 +0300

    Save point

--- CourseProject-main/DataAccess/Entities/User.cs
+++ CourseProject-main/DataAccess/Entities/User.cs
@@ -6,7 +6,7 @@ namespace DataAccess.Entities
 {
     public class User : IdentityUser
     {
-        public int Age { get; set; }
+        //public int Age { get; set; }
 
         public Guid Id { get; set; }
 
--- CourseProject-main/WebApp/Models/Users/CreateUserViewModel.cs
+++ CourseProject-main/WebApp/Models/Users/CreateUserViewModel.cs
@@ -1,17 +1,25 @@
 using System.ComponentModel.DataAnnotations;
+using System;
 
 namespace WebApp.Models.Users
 {
     public class CreateUserViewModel
     {
         [Required]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
         public string Email { get; set; }
 
         [Required]
         public string Password { get; set; }
 
-        [Required]
-        [Range(1, 100)]
-        public int? Age { get; set; }
+        //[Required]
+        //[Range(1, 100)]
+        //public int? Age { get; set; }
     }
 }
