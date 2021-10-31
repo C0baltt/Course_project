6092fa8141c52adabc8b1d5e69e7c89a032e699a
Александр Жаворонок <C0baltt@live.com>
2021-10-31 22:24:33 +0300

    Save Point

---
+++ CourseProject-main/DatabaseAccess/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DatabaseAccess.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseAccess
+{
+    public class ApplicationContext : DdContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+
+        : base(options)
+        {
+            Database.EnsureCreated();
+
+        }
+    }
+}
